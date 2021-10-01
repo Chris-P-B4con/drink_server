ab980dc73777302656cedb645c95d8fd705f52ef
Chris-P-B4con <chris.schlaiss@gmail.com>
2021-10-01 12:29:48 +0200

    Created route to add drinks to database

--- server/routes/drinks.js
+++ server/routes/drinks.js
@@ -8,23 +8,37 @@ router.get("/get", (req, res) => {
 });
 
 router.post("/add", async (req, res) => {
+  const drink = {
+    drink_name: req.body.drink_name,
+    volume: Number(req.body.volume),
+    available: Number(req.body.available),
+    price: Number(req.body.price),
+  };
   const drinks = await prisma.drinks.findMany({
     where: {
-      drink_name: req.body.drink_name,
-      volume: req.body.volume,
+      drink_name: drink.drink_name,
+      volume: drink.volume,
     },
     select: {
       drink_name: true,
     },
   });
-
-  if (!drinks) {
+  if (drinks != []) {
     const created = prisma.drinks
       .create({
-        data: req.body,
+        data: drink,
       })
-      .then();
-  }
+      .then((data) => {
+        res.status(200).json({
+          success: "Drink added to database.",
+          error: "",
+        });
+      });
+  } else
+    res.status(400).json({
+      success: "",
+      error: "Drink already in database.",
+    });
 });
 
 module.exports = router;
