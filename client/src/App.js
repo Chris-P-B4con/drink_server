0ed7049c73ebff4739a1ceac07ab6b2a4339eac5
Christian Schlaiss <chris.schlaiss@gmail.com>
2021-12-02 17:50:11 +0100

    Added profile page starter and added home link to dropdown

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import { GlobalStyles, MasterWrapper } from "./GlobalStyles";
 import dotenv from "dotenv";
@@ -7,13 +7,13 @@ import Cookies from "js-cookie";
 //Components
 import Home from "./pages/Home/Home";
 import LoginRegister from "./pages/LoginRegister/LoginRegister";
-
+import Admin from "./pages/Admin/Admin";
+import Profile from "./pages/Profile/Profile";
 function App() {
-
   React.useEffect(() => {
     document.documentElement.style.setProperty(
-      '--scrollbar-width',
-      window.innerWidth - document.documentElement.clientWidth + 'px'
+      "--scrollbar-width",
+      window.innerWidth - document.documentElement.clientWidth + "px"
     );
   }, []);
   const [user, setUser] = useState({ userId: "" });
@@ -24,11 +24,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           {sessionCookie ? (
-            // <Route path="/" element={<Home />} />
-            <Route
-              path="/"
-              element={<Home />}
-            />
+            <Fragment>
+              <Route path="/" element={<Home />} />
+              <Route path="/admin" element={<Admin />} />
+              <Route path="/profile" element={<Profile />} />
+            </Fragment>
           ) : (
             <Route path="/" element={<LoginRegister setUser={setUser} />} />
           )}
--- client/src/components/Dropdown/Dropdown.jsx
+++ client/src/components/Dropdown/Dropdown.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import DropdownItem from "./DropdownItem/DropdownItem";
 
-import { BiUser } from "react-icons/bi";
+import { BiUser, BiHomeAlt } from "react-icons/bi";
 import { RiAdminLine } from "react-icons/ri";
 import { FiLogOut } from "react-icons/fi";
 
@@ -11,14 +11,14 @@ import { Line, Wrapper } from "./DropdownStyles";
 function Dropdown(props) {
   return (
     <>
+      <DropdownItem link="/">
+        <span>
+          <BiHomeAlt />
+        </span>
+      </DropdownItem>
       <DropdownItem link="/profile">
         <span>
           <BiUser />
-        </span>
-      </DropdownItem>
-      <DropdownItem link="/admin">
-        <span>
-          <RiAdminLine />
         </span>
       </DropdownItem>
       <DropdownItem link="/logout">
